c3189821e4598c7599f5cecbc25425479d18dffa
Edmund Mok <edmundmok@outlook.com>
2018-10-19 11:43:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # privileged mode is required for ptrace
-docker run --privileged -w /cs3210/assigns/assign1 -v $(pwd)/:/cs3210 -it cs3210 /bin/bash 
+docker run --privileged -w /cs3210/assigns/assign1/part2 -v $(pwd)/:/cs3210 -it cs3210 /bin/bash 
