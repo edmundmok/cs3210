538904008996226d04c4b83cd9476969846d847f
Edmund Mok <edmundmok@outlook.com>
2018-11-06 13:33:11 +0800

    Generate partial hash input in helper

--- assigns/assign2/main.c
+++ assigns/assign2/main.c
@@ -6,8 +6,6 @@
 #include "nhash.h"
 #include "utils.h"
 
-#define NUSNET_ID "E1234567"
-
 int main() {
 
   freopen("./test.in", "r", stdin);
@@ -17,51 +15,16 @@ int main() {
   uint64_t target;
   read_inputs(prev_digest_hex_str, &target);
 
-  // Digest is 256 bits.
-  uint8_t prev_digest[32];
-  memset(prev_digest, sizeof(prev_digest), 0);
-
-  // Convert digest str to uint8_t arr
-  char mini_prev_digest[3];
-  for (int i=0; i<64; i+=2) {
-    strncpy(mini_prev_digest, prev_digest_hex_str+i, 2);
-    mini_prev_digest[2] = '\0';
-    prev_digest[i>>1] = (uint8_t) strtol(mini_prev_digest, NULL, 16);
-  }
-
   // The actual hash input can be of variable length.
   uint8_t input[52];
-  memset(input, sizeof(input), 0);
-
-  uint32_t timestamp = 0x5bb16380;
-
-  // Fill in the input
-  // 1. Fill in timestamp (starting from LSB back up)
-  for (int i=3; i>=0; i--) {
-    input[i] = timestamp & 0xff;
-    timestamp >>= 8;
-  }
-
-  // 2. Fill in the previous digest
-  for (int i=0; i<32; i++) {
-    input[i+4] = prev_digest[i];
-  }
-
-  // 3. Fill in NUSNET ID
-  for (int i=0; i<8; i++) {
-    input[i+36] = (uint8_t) NUSNET_ID[i];
-  }
+  generate_partial_hash_input(input, prev_digest_hex_str);
 
   // 4. Fill in nonce
-  char minin_nonce[3];
   uint64_t nonce = 0xe69d030000000000;
   uint64_t *nonce_ptr = input+44;
   *nonce_ptr = htobe64(nonce);
 
-  for (int i=0; i<52; i++) {
-    printf("%02x", input[i]);
-  }
-  printf("\n");
+  print_complete_hash_input(input);
 
   // Hash the input
   uint8_t hash[32];
--- assigns/assign2/utils.h
+++ assigns/assign2/utils.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <endian.h>
 
+#define NUSNET_ID "E1234567"
+
 void print_digest_prefix(uint8_t hash[]) {
   printf("The first 8 bytes of the digest are: \n");
   for (int i=0; i<8; i++) {
@@ -25,7 +27,47 @@ void read_inputs(char prev_digest_hex_str[], int *target) {
   printf("Enter previous digest (256-bit hex):\n");
   scanf("%s", prev_digest_hex_str);
   printf("Enter target value (64-bit decimal):\n");
-  scanf("%llu", &target);
+  scanf("%llu", target);
+}
+
+void print_complete_hash_input(uint8_t input[]) {
+  for (int i=0; i<52; i++) {
+    printf("%02x", input[i]);
+  }
+  printf("\n");
+}
+
+void generate_partial_hash_input(uint8_t input[], char prev_digest_hex_str[]) {
+  uint32_t timestamp = 0x5bb16380;
+
+  // Fill in the input
+  // 1. Fill in timestamp (starting from LSB back up)
+  for (int i=3; i>=0; i--) {
+    input[i] = timestamp & 0xff;
+    timestamp >>= 8;
+  }
+
+  // Digest is 256 bits.
+  uint8_t prev_digest[32];
+
+  // Convert digest str to uint8_t arr
+  char mini_prev_digest[3];
+  for (int i=0; i<64; i+=2) {
+    strncpy(mini_prev_digest, prev_digest_hex_str+i, 2);
+    mini_prev_digest[2] = '\0';
+    prev_digest[i>>1] = (uint8_t) strtol(mini_prev_digest, NULL, 16);
+  }
+
+  // 2. Fill in the previous digest
+  for (int i=0; i<32; i++) {
+    input[i+4] = prev_digest[i];
+  }
+
+  // 3. Fill in NUSNET ID
+  for (int i=0; i<8; i++) {
+    input[i+36] = (uint8_t) NUSNET_ID[i];
+  }
+
 }
 
 #endif //ASSIGN2_UTILS_H
